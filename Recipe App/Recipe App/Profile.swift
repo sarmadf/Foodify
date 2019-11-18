a8c12ed6be63e8567561a4aa4e4d030ac305bffc
StephenEdnave <LookASupernova@gmail.com>
2019-11-18 14:39:36 -0800

    Added account wireframes

---
+++ Recipe App/Recipe App/Profile.swift
@@ -0,0 +1,20 @@
+//
+//  Profile.swift
+//  Recipe App
+//
+//  Created by User on 11/18/19.
+//  Copyright © 2019 ECS 189E Group 11. All rights reserved.
+//
+
+import UIKit
+
+class Profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
